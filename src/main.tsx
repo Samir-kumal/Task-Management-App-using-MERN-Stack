c973ad6949c5b6219cec4fca3e68c8930681ba52
bhagya567 <b2.bhagya@cgim.edu.np>
2024-02-17 13:03:12 +0545

    feat: add routing for whole app using react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,57 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./routes/Login.tsx";
 import ErrorPage from "./error-page.tsx";
 import Dashboard from "./routes/Dashboard.tsx";
+import Register from "./routes/Register.tsx";
+import AuthProvider from "./context/AuthProvider.tsx";
+import DataProvider from "./context/DataProvider.tsx";
+import UserProfile from "./routes/UserProfile.tsx";
+import Root from "./routes/root.tsx";
+import ProtectedRoutes from "./routes/ProtectedRoutes.tsx";
+
+const token = localStorage.getItem("token");
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
     element: <Login />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "dashboard",
-    element: <Dashboard />,
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoutes>
+        <Dashboard />
+      </ProtectedRoutes>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoutes>
+        <UserProfile />
+      </ProtectedRoutes>
+    ),
     errorElement: <ErrorPage />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <DataProvider>
+        <RouterProvider router={router} />
+      </DataProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
