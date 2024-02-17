2beff67e7901864eea8e84f0a2dbc40d6cae9957
bhagya567 <b2.bhagya@cgim.edu.np>
2024-02-17 13:01:51 +0545

    feat: add sidebar component that shows number of boards and create new board and user profile

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import useAuthProvider from "../hooks/useAuthProvider";
+import useDataProvider from "../hooks/useDataProvider";
+import ToggleIconClose from "./Common/ToggleIconClose";
+import ToggleIconOpen from "./Common/ToggleIconOpen";
+import CreateBoard from "./Common/CreateBoardModal";
+import SignoutIcon from "./svgs/SignoutIcon";
+import ProfileIcon from "./svgs/ProfileIcon";
+import UserProfileDropDown from "./UserProfileDropDown";
+import EditIcon from "./svgs/EditIcon";
+import UpdateBoardModal from "./Common/UpdateBoardModal";
 
 interface Task {
   title: string;
@@ -12,125 +23,132 @@ interface Board {
   tasks: Task[];
   _id: string;
 }
-interface SideBarProps {
-  handleBoardClick: (boardId: string) => void;
-}
-const SideBar = ({ handleBoardClick }: SideBarProps) => {
+
+const SideBar = () => {
   const [visible, setVisible] = useState(true);
-  const [modelVisible, setModelVisible] = useState(false);
-  const [modalName, setModalName] = useState("");
+  const [createModelVisible, setcreateModelVisible] = useState(false);
+  const [updateModelVisible, setupdateModelVisible] = useState(false);
+  const [profileModalVisible, setProfileModalVisible] = useState(false);
   const [currentBoardIndex, setCurrentBoardIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+  const { user } = useAuthProvider();
+  const navigate = useNavigate();
   const handleToggle = () => {
     console.log("Toggle");
     setVisible((previous) => !previous);
   };
-  const [boards, setboards] = useState<Board[]>([]);
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWNkYWQ3YWE2ZTk4NGNjOTYzYTM0NzAiLCJ1c2VyTmFtZSI6IlNhbWlyIEt1bWFsIiwidXNlckVtYWlsIjoia3VtYWxzYW1lZXIxMjRAZ21haWwuY29tIiwiaWF0IjoxNzA3OTc4NDQ5LCJleHAiOjE3MDgyMzc2NDl9.7r2vmiyI0X0PQCwyuIGZT2W0EPWOfO9QN8Y1FSCF5aQ";
-  const fetchBoards = async () => {
-    const response = await axios.get(
-      "http://localhost:9001/boards/65cdad7aa6e984cc963a3470",
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-    const data = response.data;
+  const token = localStorage.getItem("token");
+  const { data, setBoardID, getTaskData } = useDataProvider();
+  const [selectedBoard, setSelectedBoard] = useState<Board | null>(null);
+
+  if (data) {
     console.log(data);
-    setboards(data);
+  }
+
+  const handleCreateBoardClick = async () => {
+    setcreateModelVisible(true);
+  };
+  const handleUpdateBoardClick = async (title) => {
+    setupdateModelVisible(true);
+    setSelectedBoard(title);
   };
 
-  useEffect(() => {
-    fetchBoards();
-  }, []);
-
-  console.log(boards);
-  const handleBoardCreate = async () => {
-    setModelVisible(true);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(modalName);
-    const response = await axios.post(
-      "http://localhost:9001/createBoard/65cdad7aa6e984cc963a3470",
-      { title: modalName },
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-    console.log(response);
-    setModelVisible(false);
-    fetchBoards();
-    setModalName("");
-  };
   return (
     <div
-      className={` h-[calc(100vh-4rem)] overflow bg-white shadow-2xl relative transition-all ${
+      className={` h-[calc(100vh-4rem)] overflow  bg-white shadow-2xl relative transition-all ${
         visible ? "w-60" : "w-0"
       }`}
     >
-      <div className="overflow-hidden">
-        <h1 className="font-semibold text-xl">All boards ({boards.length})</h1>
-        {boards.map((board, index) => (
-          <div className={`bg-white`} key={index}>
+      <div className="overflow-hidden flex flex-col  justify-between h-full">
+        <div className="">
+          <h1 className="font-semibold text-lg font-sans pb-4">
+            All boards ({data?.length})
+          </h1>
+          <div className="h-60 overflow-scroll ">
+            {data ? (
+              data.map((board, index) => (
+                <div
+                  onMouseEnter={() => {
+                    console.log("hovered");
+                    setIsHovered(true);
+                  }}
+                  onMouseLeave={() => {
+                    setIsHovered(false);
+                  }}
+                  className={` flex flex-row items-center ${
+                    currentBoardIndex === index
+                      ? "bg-primary  rounded-r-full"
+                      : "bg-white "
+                  }  `}
+                  key={index}
+                >
+                  <Button
+                    onClick={() => {
+                      if (token) {
+                        setCurrentBoardIndex(index);
+                        setBoardID(board._id);
+                        getTaskData(board._id, token);
+                      }
+                    }}
+                    style={`w-10/12 text-left text-sm  ${
+                      currentBoardIndex === index
+                        ? "bg-primary/90 shadow-xl text-white rounded-r-full"
+                        : "bg-white text-black/90 border-b-2"
+                    } `}
+                  >
+                    {board.title}
+                  </Button>
+                  {isHovered && (
+                    <button onClick={() => handleUpdateBoardClick(board.title)}>
+                      <EditIcon fill="#fff" width={15} height={15} />
+                    </button>
+                  )}
+                </div>
+              ))
+            ) : (
+              <div>loading....</div>
+            )}
+          </div>
+          <div className="w-full  flex flex-row justify-center">
             <Button
-              onClick={() => {
-                setCurrentBoardIndex(index);
-                handleBoardClick(board._id);
-                localStorage.setItem("boardID", board._id);
-              }}
-              style={`w-full text-left  ${
-                currentBoardIndex === index
-                  ? "bg-primary text-white"
-                  : "bg-white text-black border-b-2"
-              } `}
+              style="w-11/12 m h-fit bg-primary/60 rounded-md shadow-xl text-white"
+              onClick={handleCreateBoardClick}
             >
-              {board.title}
+              Create new board
             </Button>
           </div>
-        ))}
-        <Button style="w-full h-fit bg-gray-400" onClick={handleBoardCreate}>
-          Create new board
-        </Button>
+        </div>
+
+        <button
+          onClick={() => setProfileModalVisible((prev) => !prev)}
+          className="w-full relative flex flex-row items-center hover:bg-gray-300"
+        >
+          {profileModalVisible && <UserProfileDropDown />}
+          <div className="flex flex-row w-full justify-start p-2 gap-x-4">
+            <div className="rounded-full w-8 h-8 bg-primary flex flex-row items-center justify-center">
+              {user && user.userName.includes(" ")
+                ? user?.userName[0] + user?.userName.split(" ")[1]?.[0]
+                : user?.userName[0]}
+            </div>
+            <p>{user && user.userName}</p>
+          </div>
+        </button>
       </div>
       <Button
-        style="absolute -right-8 top-1/2 rounded-full"
+        style="absolute -right-6 top-1/4 rounded-full"
         onClick={handleToggle}
       >
-        <p>Toggle</p>
+        {visible ? <ToggleIconClose /> : <ToggleIconOpen />}
       </Button>
-      {modelVisible && (
-        <div className="inset-0 bg-black/40 absolute w-[100vw] h-[100vh] flex justify-center  ">
-          <div className="w-1/3 rounded-lg h-1/3 bg-white mt-2">
-            <div className="flex flex-row p-2 justify-center">
-              <h2 className="p-2 text-xl font-bold w-10/12 ">
-                Create New board
-              </h2>
-              <Button
-                onClick={() => setModelVisible(false)}
-                style="p-2 h-8 flex items-center justify-center rounded-md text-xs"
-              >
-                Close
-              </Button>
-            </div>
-            <form
-              onSubmit={handleSubmit}
-              className="flex w-full flex-col items-center"
-            >
-              <label htmlFor="title" className="text-left w-10/12 opacity-60">
-                Board Name
-              </label>
-              <input
-                value={modalName}
-                onChange={(e) => {
-                  setModalName(e.target.value);
-                }}
-                type="text"
-                name="title"
-                className="border-2 p-2 w-10/12 rounded-md border-black/60 my-2"
-              />
-              <Button type="submit" style="w-10/12 rounded-md">
-                Create
-              </Button>
-            </form>
-          </div>
-        </div>
+
+      {createModelVisible && (
+        <CreateBoard setModelVisible={setcreateModelVisible} />
+      )}
+      {updateModelVisible && (
+        <UpdateBoardModal
+          selectedBoard={selectedBoard}
+          setModelVisible={setupdateModelVisible}
+        />
       )}
     </div>
   );
