9ada5b2b8db1a3ccc3c4317b9274d8b8cb0da9e8
Samir-kumal <kumalsameer124@gmail.com>
2024-02-20 19:16:45 +0545

    style: align task header and description to left

--- src/components/Common/TaskBox.tsx
+++ src/components/Common/TaskBox.tsx
@@ -14,8 +14,8 @@ interface TaskBoxProps {
 const TaskBox:React.FC<TaskBoxProps> = ({ task,handleClick }) => {
   return (
     <button onClick={()=> handleClick(task)} className="mb-2 bg-white shadow-xl m-2 w-full rounded-lg p-3">
-      <h3 className="text-md font-semibold">{task.title}</h3>
-      <p className="text-xs text-gray-400 px-2 overflow-hidden">{task.content}</p>
+      <h3 className="text-md text-left font-semibold">{task.title}</h3>
+      <p className="text-xs text-left text-gray-400 px-2 overflow-hidden">{task.content}</p>
     </button>
   );
 };
