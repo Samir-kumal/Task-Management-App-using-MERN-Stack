5dcc09ebd4fb8738cf3ddbd9b39f5c033104c978
Samir-kumal <kumalsameer124@gmail.com>
2024-02-24 16:03:15 +0545

    refactor: add code for performance optimization

--- src/components/Common/ErrorTextComponent.tsx
+++ src/components/Common/ErrorTextComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 interface ErrorTextComponentProps {
   touched: boolean | undefined;
@@ -6,16 +6,18 @@ interface ErrorTextComponentProps {
   align?: string;
 }
 
-const ErrorTextComponent: React.FC<ErrorTextComponentProps> = ({
-  touched,
-  errors,
-  align,
-}) => {
-  return (
-    <p className={`text-xs text-red-500 ${align ==="center" ? "text-center" : "text-left"} w-full  h-2 -translate-y-3`}>
-      {touched && errors && errors}
-    </p>
-  );
-};
+const ErrorTextComponent: React.FC<ErrorTextComponentProps> = memo(
+  ({ touched, errors, align }) => {
+    return (
+      <p
+        className={`text-xs text-red-500 ${
+          align === "center" ? "text-center" : "text-left"
+        } w-full  h-2 -translate-y-3`}
+      >
+        {touched && errors && errors}
+      </p>
+    );
+  }
+);
 
 export default ErrorTextComponent;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Button from "./Button";
 import useAuthProvider from "../hooks/useAuthProvider";
 import useDataProvider from "../hooks/useDataProvider";
@@ -8,29 +8,37 @@ import CreateBoard from "./Common/CreateBoardModal";
 import UserProfileDropDown from "./UserProfileDropDown";
 import EditIcon from "./svgs/EditIcon";
 import UpdateBoardModal from "./Common/UpdateBoardModal";
+import LoadingComponent from "./Common/LoadingComponent";
 
 const SideBar = () => {
   const [visible, setVisible] = useState(true);
-  const [createModelVisible, setcreateModelVisible] = useState(false);
-  const [updateModelVisible, setupdateModelVisible] = useState(false);
+  const [createModelVisible, setCreateModelVisible] = useState(false);
+  const [updateModelVisible, setUpdateModelVisible] = useState(false);
   const [profileModalVisible, setProfileModalVisible] = useState(false);
   const [currentBoardIndex, setCurrentBoardIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
   const { user } = useAuthProvider();
-  const handleToggle = () => {
+  const handleToggle = useCallback(() => {
     setVisible((previous) => !previous);
-  };
+  }, [visible]);
   const token = localStorage.getItem("token");
   const { data, setBoardID, getTaskData } = useDataProvider();
   const [selectedBoard, setSelectedBoard] = useState<string | null>(null);
 
-  const handleCreateBoardClick = async () => {
-    setcreateModelVisible(true);
-  };
-  const handleUpdateBoardClick = async (title: string) => {
-    setupdateModelVisible(true);
-    setSelectedBoard(title);
-  };
+  const handleCreateBoardClick = useCallback(async () => {
+    setCreateModelVisible(true);
+    console.log("board rendered");
+  }, [createModelVisible]);
+  const handleUpdateBoardClick = useCallback(
+    async (title: string) => {
+      setUpdateModelVisible(true);
+      setSelectedBoard(title);
+    },
+    [updateModelVisible, selectedBoard]
+  );
+  const handleProfileModalClick = useCallback(() => {
+    setProfileModalVisible((previous) => !previous);
+  }, [profileModalVisible]);
 
   return (
     <div
@@ -40,11 +48,11 @@ const SideBar = () => {
     >
       <div className="overflow-hidden flex flex-col  justify-between h-full">
         <div className="">
-          <h1 className="font-semibold text-lg font-sans pb-4">
+          <h1 className="font-semibold text-lg font-sans pb-4 px-2">
             All boards ({data?.length})
           </h1>
           <div className="h-60 overflow-scroll ">
-            {data && data.length >0 ? (
+            {data && data.length > 0 ? (
               data.map((board, index) => (
                 <div
                   onMouseEnter={() => {
@@ -84,17 +92,10 @@ const SideBar = () => {
                   )}
                 </div>
               ))
+            ) : data && data.length === 0 && data !== null ? (
+              <LoadingComponent/>
             ) : (
-              <div className=" flex flex-row items-center justify-center gap-x-2">
-
-
-<div
-  className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-primary border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-  role="status">
-  
-</div>
-<p className="text-xs">Loading</p>
-              </div>
+              <p className="text-xs px-2 text-red">No board found</p>
             )}
           </div>
           <div className="w-full  flex flex-row justify-center">
@@ -107,35 +108,42 @@ const SideBar = () => {
           </div>
         </div>
 
-        <button
-          onClick={() => setProfileModalVisible((prev) => !prev)}
-          className="w-full relative flex flex-row items-center hover:bg-gray-300"
-        >
-          {profileModalVisible && <UserProfileDropDown />}
-          <div className="flex flex-row w-full justify-start p-2 gap-x-4">
-            <div className="rounded-full w-8 h-8 bg-primary flex flex-row items-center justify-center">
-              {user && user.userName.includes(" ")
-                ? user?.userName[0] + user?.userName.split(" ")[1]?.[0]
-                : user?.userName[0]}
+        <div className="h-fit flex flex-col gap-y-2">
+        {profileModalVisible && <UserProfileDropDown />}
+          <button
+            onClick={handleProfileModalClick}
+            className="w-full relative flex flex-row items-center hover:bg-gray-300"
+          >
+           
+            <div className="flex flex-row w-full justify-start p-2 gap-x-4">
+              <div className="rounded-full w-8 h-8 bg-primary flex flex-row items-center justify-center">
+                {user && user.userName.includes(" ")
+                  ? user?.userName[0] + user?.userName.split(" ")[1]?.[0]
+                  : user?.userName[0]}
+              </div>
+              <p>{user && user.userName}</p>
             </div>
-            <p>{user && user.userName}</p>
-          </div>
-        </button>
+          </button>
+        </div>
       </div>
       <Button
         style="absolute -right-6 top-1/4 rounded-full"
         onClick={handleToggle}
       >
-        {visible ? <ToggleIconClose height={30} width={30} /> : <ToggleIconOpen height={30} width={30} />}
+        {visible ? (
+          <ToggleIconClose height={30} width={30} />
+        ) : (
+          <ToggleIconOpen height={30} width={30} />
+        )}
       </Button>
 
       {createModelVisible && (
-        <CreateBoard setModelVisible={setcreateModelVisible} />
+        <CreateBoard setModelVisible={setCreateModelVisible} />
       )}
       {updateModelVisible && (
         <UpdateBoardModal
           selectedBoard={selectedBoard}
-          setModelVisible={setupdateModelVisible}
+          setModelVisible={setUpdateModelVisible}
         />
       )}
     </div>
