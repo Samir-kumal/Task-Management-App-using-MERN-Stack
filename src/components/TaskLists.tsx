9ed916bf4cc44d158623410d942a0c324b4abc5b
Samir-kumal <kumalsameer124@gmail.com>
2024-02-25 21:55:00 +0545

    style: change bg-color for each tasks

--- src/components/TaskLists.tsx
+++ src/components/TaskLists.tsx
@@ -1,8 +1,9 @@
-import {  useState } from "react";
+import { useState } from "react";
 import TaskBox from "./Common/TaskBox";
 import useDataProvider from "../hooks/useDataProvider";
 import UpdateTaskModal from "./Common/UpdateTaskModal";
 import LoadingComponent from "./Common/LoadingComponent";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export interface Task {
   _id: string;
@@ -11,15 +12,12 @@ export interface Task {
   content: string;
 }
 
-
 const TaskList = () => {
   const { tasksData } = useDataProvider();
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedTask, setSelectedTask] = useState<Task | null>(null);
 
   console.log(tasksData ?? tasksData);
-
- 
 
   const handleClick = (task: Task) => {
     setModalVisible(true);
@@ -28,13 +26,13 @@ const TaskList = () => {
 
   return (
     <>
-      <div className="w-full  h-[calc(100vh-4rem)] overflow-y-scroll px-4 grid lg:grid-cols-3 gap-x-4 md:grid-cols-2 grid-cols-1">
+      <div className="w-full  h-[calc(100vh-4rem)] overflow-y-scroll px-4 grid lg:grid-cols-3 gap-x-1 md:grid-cols-2 grid-cols-1">
         {tasksData ? (
           <>
-            <div className="">
-              <div className="flex flex-row items-center jutify-center px-4">
+            <div className="  bg-red-500/5  m-4 flex flex-col  px-2 justify-start rounded-xl">
+              <div className="flex flex-row items-center jutify-center px-4 py-2">
                 <div className={`h-3 w-3 rounded-full ${"bg-red-500"}`}></div>
-                <h2 className="font-semibold text-lg px-3 font-sans">Todo</h2>
+                <h2 className="font-semibold text-lg px-3 font-sans">Todo  </h2>
               </div>
               {tasksData
                 .filter((task: Task) => task.status === "todo")
@@ -46,8 +44,8 @@ const TaskList = () => {
                   />
                 ))}
             </div>
-            <div className="">
-              <div className="flex flex-row items-center jutify-center px-4">
+            <div className="bg-yellow-500/5  m-4 flex flex-col  px-2 justify-start rounded-xl">
+              <div className="flex flex-row items-center jutify-center px-4 py-2">
                 <div
                   className={`h-3 w-3 rounded-full ${"bg-yellow-500"}`}
                 ></div>
@@ -63,8 +61,8 @@ const TaskList = () => {
                   />
                 ))}
             </div>
-            <div className="">
-              <div className="flex flex-row items-center jutify-center px-4">
+            <div className="bg-green-500/5 m-4 flex flex-col  px-2 justify-start rounded-xl">
+              <div className="flex flex-row items-center jutify-center px-4 py-2">
                 <div className={`h-3 w-3 rounded-full ${"bg-green-500"}`}></div>
                 <h2 className="font-semibold text-lg px-3 font-sans">Done</h2>
               </div>
@@ -79,13 +77,23 @@ const TaskList = () => {
                 ))}
             </div>
           </>
-        ) : tasksData  && tasksData !== null ? (
+        ) : tasksData && tasksData !== null ? (
           <div className="w-full h-full flex justify-center items-start py-20">
             <LoadingComponent />
           </div>
         ) : (
-          <div className="w-full h-full flex justify-center  items-start py-20 ">
-            No tasks found Create a new task
+          <div className="w-lvw h-full flex flex-col justify-start  items-center -translate-x-40 py-20 ">
+            <Player
+              autoplay
+              loop
+              src="https://lottie.host/48535856-7c59-4a41-828d-4c63b82482c4/oAl6jTQFdE.json"
+              style={{ height: "200px", width: "200px" }}
+            ></Player>
+            <p className="text-2xl font-semibold">
+
+            No tasks found 
+
+            </p>
           </div>
         )}
       </div>
