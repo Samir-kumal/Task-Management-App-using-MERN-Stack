4589fb93a606be6e400cde7d3a5356a42a0126ed
Samir-kumal <kumalsameer124@gmail.com>
2024-03-03 21:53:04 +0545

    feat: add clickable taskLogo
    style: remove the add new task button add user profile option

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,43 +1,57 @@
 import { useState } from "react";
 import Button from "./Button";
-import CreateTaskModal from "./Common/TaskModal";
-import useDataProvider from "../hooks/useDataProvider";
-
+import useAuthProvider from "../hooks/useAuthProvider";
+import UserProfileDropDown from "./UserProfileDropDown";
+import Logo from "../assets/Images/TaskFlow.png"
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const { boardID } = useDataProvider();
+  const [profileModalVisible, setProfileModalVisible] = useState(false);
+
+  const {user} = useAuthProvider();
+  const navigate = useNavigate();
+
+  const handleClick = ()=>{
+    navigate("/")
+  }
+ 
 
   return (
-    <div className="h-16 w-full bg-blue-100/50  border-b-2 border-black/5 flex flex-row justify-between px-3 items-center">
+    <>
+    <div className="h-16 w-full font-poppins text-black bg-white  relative border-b-2 border-black/20 flex flex-row justify-between px-6 items-center">
       <div className="flex flex-row items-center">
-        <div>
+        <div className="">
+           <Button onClick={handleClick}>
           <img
-            src="https://img.icons8.com/ios/452/task.png"
+            src={Logo}
             alt="task"
-            className="h-6 w-6"
+            className="h-10 w-auto"
           />
+          </Button>
         </div>
-        <h1 className="font-bold lg:text-[16px] font-mono ">
-        TaskFlow
-        </h1>
+       
       </div>
-      <div>
+      <div className="">
         <Button
-          disabled={!boardID}
-          style={`rounded-full text-xs p-3 ${
-            !boardID ? "bg-black/50" : "bg-primary text-white"
-          }`}
-          onClick={() => setModalVisible(true)}
+          
+          style={`flex flex-row items-center gap-x-2`}
+          onClick={() => setProfileModalVisible(!profileModalVisible)}
         >
-          <p className="lg:flex md:flex sm:flex xs:flex hidden">
-            {" "}
-            + Add new Task
+          <p className="rounded-full text-sm  h-fit w-fit p-2 bg-primary text-white">
+          
+          {user && user.userName.includes(" ")
+                  ? user?.userName[0] + " "+ user?.userName.split(" ")[1]?.[0]
+                  : user?.userName[0]}
           </p>
-          <p className="xs:hidden sm:hidden md:hidden lg:hidden flex">+Add</p>
+          <p>
+            {user?.userName}
+          </p>
         </Button>
+        {profileModalVisible && <div className="absolute right-2 -bottom-24 z-50"><UserProfileDropDown /></div>}
       </div>
-      {modalVisible && <CreateTaskModal setModalVisible={setModalVisible} />}
+      
     </div>
+   
+    </>
   );
 };
 
