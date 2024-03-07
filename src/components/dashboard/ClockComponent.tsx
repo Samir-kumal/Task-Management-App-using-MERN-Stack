7f35cb3c58e2a18e0466aee95d9defc959e6de50
Samir-kumal <kumalsameer124@gmail.com>
2024-03-07 13:16:42 +0545

    feat: add clock component for time

---
+++ src/components/dashboard/ClockComponent.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const ClockComponent = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(interval);
+    })
+  return (
+    <div className="h-40 w-1/2 bg-white rounded-xl p-4 font-poppins font-semibold">
+     <h1 className="text-xl"> Clock</h1>
+      <div className = "w-full h-full flex flex-row items-start justify-start">
+      <p className = "text-4xl  mt-6">
+        {time}
+      </p>
+      </div>
+    </div>
+  );
+};
+
+export default ClockComponent;
