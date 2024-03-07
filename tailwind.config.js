81325bc00f89fdd8f1b0c6dcb2ea76806f35f484
Samir-kumal <kumalsameer124@gmail.com>
2024-03-07 13:18:06 +0545

    chore(deps): add daisyui in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundColor: {
-        'primary': '#7994F2',
-        'secondary': '#4b5563',
-        'custom-color-1': '#ff9900',
-        'custom-color-2': '#9900ff',
+        primary: "#7994F2",
+        primaryColor: "#7994F2",
+        secondary: "#4b5563",
+     
       },
       textColor: {
-        'primary': '#7994F2',
-        'secondary': '#4b5563',
-        'custom-color-1': '#ff9900',
-        'custom-color-2': '#9900ff',
+        primary: "#7994F2",
+        secondary: "#4b5563",
+       
       },
       borderColor: {
-        'primary': '#7994F2',
-        'secondary': '#4b5563',
-        'custom-color-1': '#ff9900',
-        'custom-color-2': '#9900ff',
+        primary: "#7994F2",
+        secondary: "#4b5563",
+    
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 
+  daisyui: {
+    themes: ["cupcake"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+};
