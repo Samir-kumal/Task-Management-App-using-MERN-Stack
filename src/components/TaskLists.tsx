793b28b15b1af2837c00f62c41ac70ba2c32a049
Samir-kumal <kumalsameer124@gmail.com>
2024-03-07 14:55:55 +0545

    feat: add task create and update

--- src/components/TaskLists.tsx
+++ src/components/TaskLists.tsx
@@ -1,109 +1,105 @@
-import { useState } from "react";
+import {useState } from "react";
 import TaskBox from "./Common/TaskBox";
 import useDataProvider from "../hooks/useDataProvider";
 import UpdateTaskModal from "./Common/UpdateTaskModal";
-import LoadingComponent from "./Common/LoadingComponent";
-import { Player } from "@lottiefiles/react-lottie-player";
 
-export interface Task {
-  _id: string;
-  status: string;
-  title: string;
-  content: string;
-}
+import Button from "./Button";
+import TaskModal from "./Common/TaskModal";
+import useFetchAllTasks from "../hooks/useFetchAllTasks";
+import LoadingComponent from "./Common/LoadingComponent";
+import { Task } from "../context/DataProvider";
+import { useParams } from "react-router-dom";
+
+
 
 const TaskList = () => {
-  const { tasksData } = useDataProvider();
+  const { tasksData, isLoading } = useDataProvider();
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [createTaskModalVisible, setCreateTaskModalVisible] = useState(false);
+  console.log(tasksData?.length);
+  const {boardID} = useParams();
 
-  console.log(tasksData ?? tasksData);
 
   const handleClick = (task: Task) => {
     setModalVisible(true);
     setSelectedTask(task);
   };
+  const { tasks } = useFetchAllTasks("boardTask");
+  const { todoTasks, doingTasks, doneTasks } = tasks;
 
   return (
-    <>
-      <div className="w-full  h-[calc(100vh-4rem)] overflow-y-scroll px-4 grid lg:grid-cols-3 gap-x-1 md:grid-cols-2 grid-cols-1">
-        {tasksData ? (
+    <div className="flex flex-col w-full  p-4 font-poppins">
+      <div className="px-8">
+        <h1 className="text-2xl text-black font-semibold font-poppins">
+          Task Lists
+        </h1>
+      </div>
+      <div className=" text-black  h-[calc(100vh-4rem)] overflow-y-scroll relative px-4 grid lg:grid-cols-3 gap-x-1 md:grid-cols-2 grid-cols-1">
+        {(tasksData && tasksData.length > 0) ||
+        (tasksData?.length == 0 && !isLoading) ? (
           <>
-            <div className="  bg-red-500/5  m-4 flex flex-col  px-2 justify-start rounded-xl">
-              <div className="flex flex-row items-center jutify-center px-4 py-2">
+            <div className="  bg-red-500/5 h-fit m-4 flex flex-col  px-2 justify-start rounded-xl">
+              <div className="flex flex-row items-center  px-4 py-2">
                 <div className={`h-3 w-3 rounded-full ${"bg-red-500"}`}></div>
-                <h2 className="font-semibold text-lg px-3 font-sans">Todo  </h2>
+                <h2 className="font-semibold text-lg px-3 ">
+                  Todo ({todoTasks?.length}){" "}
+                </h2>
               </div>
-              {tasksData
-                .filter((task: Task) => task.status === "todo")
-                .map((task: Task) => (
-                  <TaskBox
-                    handleClick={handleClick}
-                    key={task._id}
-                    task={task}
-                  />
-                ))}
+              {todoTasks?.map((task: Task) => (
+                <TaskBox handleClick={handleClick} key={task._id} task={task} />
+              ))}
+              <Button
+                onClick={() =>
+                  setCreateTaskModalVisible(!createTaskModalVisible)
+                }
+                style="shadow-xl rounded-xl bg-primary text-white"
+              >
+                + Add task
+              </Button>
             </div>
-            <div className="bg-yellow-500/5  m-4 flex flex-col  px-2 justify-start rounded-xl">
-              <div className="flex flex-row items-center jutify-center px-4 py-2">
+            <div className="bg-yellow-500/5 h-fit  m-4 flex flex-col  px-2 justify-start rounded-xl">
+              <div className="flex flex-row items-center  px-4 py-2">
                 <div
                   className={`h-3 w-3 rounded-full ${"bg-yellow-500"}`}
                 ></div>
-                <h2 className="font-semibold text-lg px-3 font-sans">Doing</h2>
+                <h2 className="font-semibold text-lg px-3">
+                  Doing ({doingTasks?.length})
+                </h2>
               </div>
-              {tasksData
-                .filter((task: Task) => task.status === "doing")
-                .map((task: Task) => (
-                  <TaskBox
-                    handleClick={handleClick}
-                    key={task._id}
-                    task={task}
-                  />
-                ))}
+              {doingTasks?.map((task: Task) => (
+                <TaskBox handleClick={handleClick} key={task._id} task={task} />
+              ))}
             </div>
-            <div className="bg-green-500/5 m-4 flex flex-col  px-2 justify-start rounded-xl">
+            <div className="bg-green-500/5 h-fit m-4 flex flex-col  px-2 justify-start rounded-xl">
               <div className="flex flex-row items-center jutify-center px-4 py-2">
                 <div className={`h-3 w-3 rounded-full ${"bg-green-500"}`}></div>
-                <h2 className="font-semibold text-lg px-3 font-sans">Done</h2>
+                <h2 className="font-semibold text-lg px-3">
+                  Done ({doneTasks?.length})
+                </h2>
               </div>
-              {tasksData
-                .filter((task: Task) => task.status === "done")
-                .map((task: Task) => (
-                  <TaskBox
-                    handleClick={handleClick}
-                    key={task._id}
-                    task={task}
-                  />
-                ))}
+              {doneTasks?.map((task: Task) => (
+                <TaskBox handleClick={handleClick} key={task._id} task={task} />
+              ))}
             </div>
           </>
-        ) : tasksData && tasksData !== null ? (
-          <div className="w-full h-full flex justify-center items-start py-20">
-            <LoadingComponent />
-          </div>
         ) : (
-          <div className="w-lvw h-full flex flex-col justify-start  items-center -translate-x-40 py-20 ">
-            <Player
-              autoplay
-              loop
-              src="https://lottie.host/48535856-7c59-4a41-828d-4c63b82482c4/oAl6jTQFdE.json"
-              style={{ height: "200px", width: "200px" }}
-            ></Player>
-            <p className="text-2xl font-semibold">
-
-            No tasks found 
-
-            </p>
+          <div>
+            <LoadingComponent />
           </div>
         )}
       </div>
       {modalVisible && (
         <UpdateTaskModal
+        boardID = {boardID ? boardID : ""} 
           setModalVisible={setModalVisible}
           selectedTask={selectedTask}
         />
       )}
-    </>
+      {createTaskModalVisible &&  (
+        <TaskModal setModalVisible={setCreateTaskModalVisible} boardID = {boardID ? boardID : ""}  />
+      )}
+    </div>
   );
 };
 
