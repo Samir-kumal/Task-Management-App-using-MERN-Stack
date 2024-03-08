e83e88728e74a82d9d452ef7b747e00c09ae2317
Samir-kumal <kumalsameer124@gmail.com>
2024-03-08 18:38:47 +0545

    chore: remove unwanted code from dataContext

--- src/hooks/useDataProvider.ts
+++ src/hooks/useDataProvider.ts
@@ -6,11 +6,7 @@ const useDataProvider = () => {
   if (!context) {
     throw new Error("useDataProvider must be used within a DataProvider");
   }
-  const { data } = context;
-  if (data && data[0] && data[0]._id) {
-    const boardId = data[0]._id;
-    localStorage.setItem("boardID", boardId);
-  }
+
   return context;
 };
 
