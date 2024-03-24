ea41325225bcb23727fc529b5edd3a5234888529
Samir kumal <kumalsameer124@gmail.com>
2024-03-24 12:37:02 +0545

    feat: add stroll to top in each route

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,4 +1,5 @@
 
+import { useLayoutEffect } from "react";
 import useAuthProvider from "../hooks/useAuthProvider";
 
 
@@ -7,5 +8,8 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 export default function ProtectedRoutes() {
   const { isLoggedIn } = useAuthProvider();
   const location = useLocation();
+  useLayoutEffect(()=>{
+    window.scrollTo(0,0);
+  },[location.pathname])
   return isLoggedIn  ? <Outlet context={isLoggedIn} /> : <Navigate to="/login" state={{from:location}}  replace />;
 }
